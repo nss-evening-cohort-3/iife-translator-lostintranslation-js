aa310dbc4fb4529833cb42af83fda270942ef165
Sylvia Ma <xiangwma@umich.edu>
2016-02-25 22:39:43 -0600

    added eventhandler.js and italian.js,for index.js, I deleted the formtag and addedd id for the input

---
+++ eventhandler.js
@@ -0,0 +1,32 @@
+//TRANSLATE FUNCTION
+document.getElementById("transbtn").addEventListener("click", translate);
+
+function translate(){
+  if(document.getElementById("french").checked){
+    Translator.transFrench();
+  }
+  else if(document.getElementById("italian").checked){
+    Translator.transItalian();
+
+  }
+  else if(document.getElementById("spanish").checked){
+  Translator.transSpanish();
+  }
+  else {
+    Translator.transGerman(); 
+  }
+}
+
+//CLEAR FUNCTION
+document.getElementById("clr").addEventListener("click", clear);
+
+function clear(){
+  //to clear the input value
+  document.getElementById("input").value="";
+
+  //to clear the radio button value
+  var ele=document.getElementsByName("lang");
+  for (i=0;i<ele.length;i++){
+    ele[i].checked = false;
+  }
+}
