e661a7f48c0e4c9c9a8700b5ee0da33df43bd84c
MATT BRUTON <the.bruton@gmail.com>
2016-02-27 09:05:45 -0600

    made changes to lexicon object in js file

--- french.js
+++ french.js
@@ -1,17 +1,32 @@
 var Translator = (function(originalTranslator) {
-  var lexicon = {
-    merry: "help",
-    christmas: "",
-    happy: "",
-    holidays: "",
-    super: "",
-    baby: "",
-    jesus: "",
-    day: ""
+
+  var frenchLexicon = {
+    merry: "Joyeux",
+    christmas: "Noël",
+    happy: "Bon",
+    holidays: "vacances",
+    super: "supérieur",
+    baby: "bébé",
+    jesus: "Jésus",
+    day: "jour",
+    and: "et",
+    new: "Nouveau",
+    year: "année",
   };
   
-  originalTranslator.getLexicon = function() {
-    return lexicon;
-  };
+  originalTranslator.transFrench = function(){
+
+    var originalText= document.getElementById("input").value.toLowerCase(); 
+    var transArray = originalText.split(" ");
+    var translatedGreeting = "";
+
+    for(i=0; i<transArray.length; i++) {
+        translatedGreeting += frenchLexicon[transArray[i]]+ " ";
+      };
+
+    document.getElementById("translatedText").innerHTML = translatedGreeting;
+    }
+  
+
   return originalTranslator;
 } (Translator || {}));
