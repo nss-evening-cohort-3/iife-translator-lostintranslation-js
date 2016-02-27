3150fe080c0f7c81981b88455b485d7c135cbdf8
utleroy <bryanhankins@me.com>
2016-02-27 13:06:39 -0600

    Modified to display user input if the word they type if it is not in the pre determined array

--- spanish.js
+++ spanish.js
@@ -8,20 +8,49 @@ var Translator = (function(originalTranslator) {
   	A: "un",
   	HAPPY: "Contento",
   	NEW: "nuevo",
-  	YEAR: "ano"
+  	YEAR: "ano",
+    HOLY: "¡ santo",
+    SHIT: "mierda",
+    WHERE: "donde",
+    IS: "es",
+    THE: "el",
+    TYLENOL: "tylenol",
   }
 
   originalTranslator.transSpanish = function() {
-    var caps= document.getElementById("input").value.toUpperCase(); 
+    var caps= document.getElementById("input").value.toUpperCase();
+    console.log(caps)
+    // var notTrans =  
     var translation = caps.split(" "); 
     var answer = "";
 
      for(i=0; i<translation.length; i++) {
-     answer += commentPair[translation[i]]+ " ";
+      if (commentPair[translation[i]] === undefined) {
+        answer += translation[i].toLowerCase() + " ";
+      } else {
+        answer += commentPair[translation[i]]+ " ";  
+      }
+     
     }
+
+    
+
+    // function returnEng(originalTranslator) {
+    //   var userEng = originalTranslator.getElementById("translatedText");
+    //   if (input.value === commentPair) {
+    //     return translatedText;
+
+    //   } else {
+    //     return returnEng;
+    //   }
+    // }
+
+    // document.getElementById("translatedText").innerHTML = userEng;
 
     document.getElementById("translatedText").innerHTML = answer;
     }
+
+
   
   return originalTranslator;  
 })(Translator||{});
